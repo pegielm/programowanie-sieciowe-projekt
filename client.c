889e83fed773b243fd6abe516e864b5198b4a311
Jakub Pomorski <delveller1@gmail.com>
2025-06-23 15:25:55 +0200

    cosmetic changes

--- client.c
+++ client.c
@@ -8,14 +8,18 @@
 #include <arpa/inet.h>
 
 int main(int argc, char *argv[]) {
-    if (argc<2) return 1;
+    if (argc < 2) return 1;
+
     int s = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr;
     memset(&addr, 0, sizeof(addr));
+
     addr.sin_family = AF_INET;
     addr.sin_port = htons(12345);
     inet_pton(AF_INET, argv[1], &addr.sin_addr);
+
     if (connect(s, (struct sockaddr*)&addr, sizeof(addr))) return 1;
+
     FILE *f = fdopen(s, "r+");
     char buf[256];
     while (fgets(buf, sizeof(buf), f)) {
